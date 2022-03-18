23854b7a43541f5b3d8c75386fa087290a187422
DESKTOP-BKAM140\Yariel Silva <yariel.usher@gmail.com>
2022-03-18 20:46:29 -0300

    changed client id to connect to Mercado Libre

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   URI_ROOT: 'https://dev.pepeganga-api.com/pepeganga',
-  URI_MELI: `http://auth.mercadolibre.com.uy/authorization?response_type=code&client_id=3756313271086728&state=${Math.floor(Math.random() * 100)}&redirect_uri=https://drop-beeseller.web.app/home/meli-accounts`,
+  URI_MELI: `http://auth.mercadolibre.com.uy/authorization?response_type=code&client_id=648079461510645&state=${Math.floor(Math.random() * 100)}&redirect_uri=https://drop-beeseller.web.app/home/meli-accounts`,
   URI_MELI_PUBLIC: 'https://api.mercadolibre.com',
   URI_RESET_PASS: 'https://drop-beeseller.web.app/auth/reset',
   URI_UPLOAD_BUCKET: 'https://dev.pepeganga-api.com/pepeganga/upload/api/bucket/download-file-from-upload-bucket?pathFile='
