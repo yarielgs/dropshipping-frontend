1ab58cae9bde9f4d7066463c66806ccd66226577
darwinfont2022 <darwinfont2022@gmail.com>
2023-03-02 15:22:41 -0300

    remove view marketplaces

--- src/app/home/services/sidebar.service.ts
+++ src/app/home/services/sidebar.service.ts
@@ -14,7 +14,7 @@ export class SidebarService {
       subtitle: 'Productos',
       divider: 'sidebar-divider',
       submenu: [
-        { title: 'Listar productos', url: '/home/store', roles: [RoleEnum.ADMIN, RoleEnum.SELLER, RoleEnum.INVITED]}
+        { title: 'Listar productos', url: '/home/store', roles: [RoleEnum.ADMIN, RoleEnum.SELLER, RoleEnum.INVITED] }
       ]
     },
     {
@@ -23,7 +23,7 @@ export class SidebarService {
       subtitle: 'Mis productos',
       divider: 'sidebar-divider',
       submenu: [
-        { title: 'Mis productos', url: '/home/marketplaces' , roles: [RoleEnum.ADMIN, RoleEnum.SELLER]},
+        { title: 'Mis productos', url: '/home/publish-myproducts', roles: [RoleEnum.ADMIN, RoleEnum.SELLER] },
         { title: 'Productos Publicados', url: '/home/published-products', roles: [RoleEnum.ADMIN, RoleEnum.SELLER] }
       ]
     },
@@ -33,9 +33,9 @@ export class SidebarService {
       subtitle: 'Ventas',
       divider: 'sidebar-divider',
       submenu: [
-        { title: 'Órdenes', url: '/home/seller-orders' , roles: [RoleEnum.ADMIN, RoleEnum.SELLER]},
-        { title: 'Operaciones', url: '/home/operations' , roles: [RoleEnum.ADMIN, RoleEnum.OPERATOR]},
-        { title: 'Histórico-Operaciones', url: '/home/historical-operations' , roles: [RoleEnum.ADMIN, RoleEnum.OPERATOR]}
+        { title: 'Órdenes', url: '/home/seller-orders', roles: [RoleEnum.ADMIN, RoleEnum.SELLER] },
+        { title: 'Operaciones', url: '/home/operations', roles: [RoleEnum.ADMIN, RoleEnum.OPERATOR] },
+        { title: 'Histórico-Operaciones', url: '/home/historical-operations', roles: [RoleEnum.ADMIN, RoleEnum.OPERATOR] }
       ],
     },
     {
@@ -45,11 +45,11 @@ export class SidebarService {
       divider: 'sidebar-divider',
       submenu: [
         { title: 'Usuarios', url: '/home/users-admin', roles: [RoleEnum.ADMIN] },
-        { title: 'Vendedores', url: '/home/sellers' , roles: [RoleEnum.ADMIN]},
+        { title: 'Vendedores', url: '/home/sellers', roles: [RoleEnum.ADMIN] },
         { title: 'Márgenes', url: '/home/margins', roles: [RoleEnum.ADMIN, RoleEnum.SELLER] },
-        { title: 'Marketplaces', url: '/home/list-marketplaces' , roles: [RoleEnum.ADMIN]},
-        { title: 'Cuentas Mercado Libre', url: '/home/meli-accounts' , roles: [RoleEnum.ADMIN, RoleEnum.SELLER]},
-        { title: 'Configuración del sistema', url: '/home/configuration' , roles: [RoleEnum.ADMIN]}
+        { title: 'Marketplaces', url: '/home/list-marketplaces', roles: [RoleEnum.ADMIN] },
+        { title: 'Cuentas Mercado Libre', url: '/home/meli-accounts', roles: [RoleEnum.ADMIN, RoleEnum.SELLER] },
+        { title: 'Configuración del sistema', url: '/home/configuration', roles: [RoleEnum.ADMIN] }
       ]
     }
   ];
