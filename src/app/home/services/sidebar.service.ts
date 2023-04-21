287c1abee6480732154012c3638ae362c588bfb9
Tiana Gerfauo <tiana@bee-seller.com>
2023-04-21 13:49:01 -0300

    Cambio en el orden de sidebar

--- src/app/home/services/sidebar.service.ts
+++ src/app/home/services/sidebar.service.ts
@@ -9,6 +9,20 @@ export class SidebarService {
 
   storeMenu: any = [
     {
+      title: 'Configuración',
+      icon: 'fas fa-user-lock',
+      subtitle: 'Panel',
+      divider: 'sidebar-divider',
+      submenu: [
+        { title: 'Usuarios', url: '/home/users-admin', roles: [RoleEnum.ADMIN] },
+        { title: 'Vendedores', url: '/home/sellers', roles: [RoleEnum.ADMIN] },
+        { title: 'Márgenes', url: '/home/margins', roles: [RoleEnum.ADMIN, RoleEnum.SELLER] },
+        { title: 'Marketplaces', url: '/home/list-marketplaces', roles: [RoleEnum.ADMIN] },
+        { title: 'Cuentas Mercado Libre', url: '/home/meli-accounts', roles: [RoleEnum.ADMIN, RoleEnum.SELLER] },
+        { title: 'Configuración del sistema', url: '/home/configuration', roles: [RoleEnum.ADMIN] }
+      ]
+    },
+    {
       title: 'Almacén Bee Seller',
       icon: 'fas fa-warehouse',
       subtitle: 'Productos',
@@ -38,20 +52,6 @@ export class SidebarService {
         { title: 'Histórico-Operaciones', url: '/home/historical-operations', roles: [RoleEnum.ADMIN, RoleEnum.OPERATOR] }
       ],
     },
-    {
-      title: 'Administración',
-      icon: 'fas fa-user-lock',
-      subtitle: 'Panel',
-      divider: 'sidebar-divider',
-      submenu: [
-        { title: 'Usuarios', url: '/home/users-admin', roles: [RoleEnum.ADMIN] },
-        { title: 'Vendedores', url: '/home/sellers', roles: [RoleEnum.ADMIN] },
-        { title: 'Márgenes', url: '/home/margins', roles: [RoleEnum.ADMIN, RoleEnum.SELLER] },
-        { title: 'Marketplaces', url: '/home/list-marketplaces', roles: [RoleEnum.ADMIN] },
-        { title: 'Cuentas Mercado Libre', url: '/home/meli-accounts', roles: [RoleEnum.ADMIN, RoleEnum.SELLER] },
-        { title: 'Configuración del sistema', url: '/home/configuration', roles: [RoleEnum.ADMIN] }
-      ]
-    }
   ];
 
 
