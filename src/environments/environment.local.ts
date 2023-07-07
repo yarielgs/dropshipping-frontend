b9915065da6bd136bea0046df92fc181f0dd42d5
darwinfont2022 <darwinfont2022@gmail.com>
2023-07-07 12:04:21 -0300

    Fix Publication error [Marca]

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -4,10 +4,10 @@
 
 export const environment = {
   production: false,
-  URI_ROOT: 'http://localhost:9999/pepeganga',
-  URI_MELI: `http://auth.mercadolibre.com.uy/authorization?response_type=code&client_id=5381382874135569&state=${Math.floor(Math.random() * 100)}&redirect_uri=https://localhost:4200/home/meli-accounts`,
+  URI_ROOT: 'https://api.dropshipping.bee-seller.com/pepeganga',
+  URI_MELI: `http://auth.mercadolibre.com.uy/authorization?response_type=code&client_id=1737771850020695&state=${Math.floor(Math.random() * 100)}&redirect_uri=https://dropshipping.bee-seller.com/home/meli-accounts`,
   URI_MELI_PUBLIC: 'https://api.mercadolibre.com',
-  URI_RESET_PASS: 'https://localhost:4200/auth/reset',
+  URI_RESET_PASS: 'https://dropshipping.bee-seller.com/auth/reset',
   URI_UPLOAD_BUCKET: 'https://api.dropshipping.bee-seller.com/pepeganga/upload/api/bucket/download-file-from-upload-bucket?pathFile='
 };
 
